d9056b1a7f7733176a8487b31f3a7d7caa55373d
Daniel Prescott <DanielEPrescott@gmail.com>
2018-04-04 14:34:08 -0600

    Part E

--- TablePartE.py
+++ TablePartE.py
@@ -2,8 +2,8 @@ import happybase as hb
 
 conn = hb.Connection()
 
-table = conn.table('emp')
-columns = (b'personal:name', b'personal:city')
+table = conn.table('powers')
+columns = (b'personal:hero', b'personal:power', b'professional:name', b'professional:xp', b'custom:color')
 
 for key, data in table.scan(columns=columns, include_timestamp=True):
     print('Found: {}, {}'.format(key, data))
