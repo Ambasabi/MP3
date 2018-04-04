55f93c5c74aaa4e5705965fe94869f4e3726c6b4
Daniel Prescott <DanielEPrescott@gmail.com>
2018-04-04 14:20:39 -0600

    Table Part D done

--- TablePartD.py
+++ TablePartD.py
@@ -13,4 +13,17 @@ color = row[b'custom:color']
 
 print('hero: {} power: {} name: {} xp: {}, color: {}'.format(hero, power, name, xp, color))
 
+row = table.row(b'row19')
+hero = row[b'personal:hero']
+color = row[b'custom:color']
+
+print('hero: {}, color: {}'.format(hero, color))
+
+row = table.row[b'row1']
+hero = row[b'personal:hero']
+name = row[b'professional:name']
+color = row[b'customer:color']
+
+print('hero: {}, name: {}, color: {}'.format(hero, name, color))
+
 conn.close()
